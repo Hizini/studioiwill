811afa8168ad1ed74ae54224a2eac9cd430780cd
Hizini <ejfdnrl@naver.com>
2023-06-20 13:47:14 +0900

    push

--- src/About/About.js
+++ src/About/About.js
@@ -1,78 +1,108 @@
-import './About.css'
-import Nav from '../Nav/Nav'
-import React, { useEffect, useRef, useState } from 'react'
-import Slider from "react-slick"
-import "slick-carousel/slick/slick.css"
-import "slick-carousel/slick/slick-theme.css"
+import "./About.scss";
+import Nav from "../Nav/Nav";
+import React, { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const previewImges = [
-  {
-    src: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLLREg%2FbtrbhYLdx4M%2Fw3BWXIjm185YfkGITrU9HK%2Fimg.jpg",
-  },
-  {
-    src: "https://pbs.twimg.com/media/FIbhNioakAAc_V0?format=jpg&name=medium",
-  },
-  {
-    src: "https://pbs.twimg.com/media/FdFxDv2aEAMqRfK?format=jpg&name=small",
-  },
-  {
-    src: "https://pbs.twimg.com/media/Ezq61WWVcAESp7b.jpg",
-  },
-]
+    { src: "/images/about/001.jpg" },
+    { src: "/images/about/002.jpg" },
+    { src: "/images/about/003.jpg" },
+    { src: "/images/about/004.jpg" },
+    { src: "/images/about/005.jpg" },
+    { src: "/images/about/006.gif" },
+    { src: "/images/about/007.jpg" },
+    { src: "/images/about/008.jpg" },
+    { src: "/images/about/009.jpg" },
+    { src: "/images/about/010.jpg" },
+    { src: "/images/about/011.jpg" },
+];
 
 const About = () => {
-  const [previewSrc, setPreviewSrc] = useState("https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbLLREg%2FbtrbhYLdx4M%2Fw3BWXIjm185YfkGITrU9HK%2Fimg.jpg")
+    const [previewSrc, setPreviewSrc] = useState(
+        "/images/about/001.jpg"
+    );
 
-  const slickRef = useRef()
+    const slickRef = useRef();
 
-  useEffect(() => {
-    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' })
-  }, [])
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    }, []);
 
-  const handleClickPrvBtn = () => slickRef.current.slickPrev()
-  const handleClickNextBtn = () => slickRef.current.slickNext()
+    const handleClickPrvBtn = () => slickRef.current.slickPrev();
+    const handleClickNextBtn = () => slickRef.current.slickNext();
 
-  const handleClickPreview = (src) => {
-    setPreviewSrc(src)
-  }
+    const handleClickPreview = (src) => {
+        setPreviewSrc(src);
+    };
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    variableWidth: true,
-    arrows: false
-  }
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        arrows: false,
+    };
 
-  return (
-    <>
-      <Nav />
-      <div className='studioiwill-About-root-container'>
-        <div className='preview-area'>
-          <img className='preview' src={previewSrc} alt='' />
-          <div className='preview-slider-area'>
-            <div className='preview-slider'>
-              <Slider {...settings} ref={slickRef}>
-                {
-                  previewImges.map((img, i) => <img className='preview-img' onClick={() => handleClickPreview(img.src)} src={img.src} alt='' />)
-                }
-              </Slider>
-              <div className='about-slider-btn prev'><img className='slider-btn-img' onClick={handleClickPrvBtn} src='/images/left-arrow.svg' alt='' /></div>
-              <div className='about-slider-btn next'><img className='slider-btn-img' onClick={handleClickNextBtn} src='/images/right-arrow.svg' alt='' /></div>
+    return (
+        <>
+            <Nav />
+            <div className="studioiwill-About-root-container">
+                <div className="preview-area">
+                    <img className="preview" src={previewSrc} alt="" />
+                    <div className="preview-slider-area">
+                        <div className="preview-slider">
+                            <Slider {...settings} ref={slickRef}>
+                                {previewImges.map((img, i) => (
+                                    <img
+                                        className="preview-img"
+                                        onClick={() =>
+                                            handleClickPreview(img.src)
+                                        }
+                                        src={img.src}
+                                        alt=""
+                                    />
+                                ))}
+                            </Slider>
+                            <div className="about-slider-btn prev">
+                                <img
+                                    className="slider-btn-img"
+                                    onClick={handleClickPrvBtn}
+                                    src="/images/left-arrow.svg"
+                                    alt=""
+                                />
+                            </div>
+                            <div className="about-slider-btn next">
+                                <img
+                                    className="slider-btn-img"
+                                    onClick={handleClickNextBtn}
+                                    src="/images/right-arrow.svg"
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="about-area">
+                    <div className="about-logo-area">
+                        <img
+                            className="logo"
+                            src="/images/studioiwill-logo2.svg"
+                            alt=""
+                        />
+                    </div>
+                    <div className="about-text">I'll make anything for you</div>
+                    <div className="about-text2">
+                        서울특별시 서대문구 연희로57 2F
+                    </div>
+                    <div className="about-text2">김희원, 한덕규</div>
+                </div>
             </div>
-          </div>
-        </div>
-        <div className='about-area'>
-          <div className='about-logo-area'><img className='logo' src='/images/studioiwill-logo2.svg' alt='' /></div>
-          <div className='about-text'>I'll make anything for you</div>
-          <div className='about-text2'>서울특별시 서대문구 연희로57 2F</div>
-          <div className='about-text2'>김희원, 한덕규</div>
-        </div>
-      </div>
-    </>
-  )
-}
+        </>
+    );
+};
 
-export default About
+export default About;
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,7 +6,7 @@ const Nav = (props) => {
         <div className='nav-container'>
             <NavLink to={'/'} className='nav-logo-area'><img className='logo' src='/images/studioiwill-small-logo.png' alt=''/></NavLink>
             <div className='menu-area'>
-                <NavLink to={'/about'} className='menu-item'>에 대한</NavLink>
+                <NavLink to={'/about'} className='menu-item'>~에 대한</NavLink>
                 {/* <NavLink className='menu-item'>프로젝트</NavLink> */}
                 <NavLink className='menu-item'>연락하다</NavLink>
                 <NavLink className='menu-item'><img className='instagram' src='/images/instagram-logo.webp' alt=''/></NavLink>
